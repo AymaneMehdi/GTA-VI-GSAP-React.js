dc45216629716ca4dd44e15f7cce44ba7a6e1278
Aymane Mehdi <yorubornin1999@gmail.com>
2026-07-05 23:56:29 +0100

    Add First Video section with GSAP animations and video playback.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
 import NavBar from "./sections/NavBar";
 import Hero from "./sections/Hero";
+import FirstVideo from "./sections/FirstVideo";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
   return (
     <main>
       <NavBar />
       <Hero />
+      <FirstVideo />
     </main>
   );
 }
